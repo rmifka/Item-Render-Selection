3533839455e0ad52e01a7cc1b527beb2451ec0f8
Mifka Rene <rene.mifka@gmail.com>
2025-08-05 01:45:39 +0200

    Change render suppression method to maybe prevent lag

---
+++ src/client/java/com/renschi/irs/mixin/client/BlockEntityRenderDispatcherMixin.java
@@ -0,0 +1,28 @@
+package com.renschi.irs.mixin.client;
+
+import com.renschi.irs.RegexFilterManager;
+import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityRenderDispatcher.class)
+public class BlockEntityRenderDispatcherMixin {
+
+    @Inject(
+            method = "shouldRender",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private <E extends Entity> void onShouldRenderEntity(E entity, Frustum frustum, double d, double e, double f, CallbackInfoReturnable<Boolean> ci) {
+        String name = entity.getName()
+                .getString();
+        if (RegexFilterManager.shouldHide(name)) {
+            ci.setReturnValue(false);
+        }
+    }
+}
+
